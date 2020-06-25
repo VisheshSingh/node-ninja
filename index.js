8af4c8d6cb9bc13ae80379273792921ed2ec18e1
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-24 22:06:55 -0500

    modules & require 🤸‍♂️

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log(__dirname);
 console.log(__filename);
+
+const { people, ages } = require('./people');
+
+console.log(people, ages);
---
+++ people.js
@@ -0,0 +1,3 @@
+const people = ['mario', 'luigi', 'princess peach', 'bowser'];
+const ages = [25, 20, 18, 35];
+module.exports = { people, ages };
