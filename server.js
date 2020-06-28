46543ea3648f001ac5b3c9910e62bdb927f7df91
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-27 23:21:53 -0500

    res object 🎇

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<head><link rel="stylesheet" href="#"/></head>');
+  res.write('<h1>hello, ninjas!</h1>');
+  res.write('<p>hey gang</p>');
+
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
