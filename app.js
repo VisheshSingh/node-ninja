9d56fc3f4afc167938567cc2793a625211cdb3b7
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-29 20:28:42 -0500

    routing & html pages 🍩

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`<p>home page</p>`);
+  //   res.send(`<p>home page</p>`);
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  //   res.send(`<p>home page</p>`);
+  res.sendFile('./views/about.html', { root: __dirname });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000...'));
