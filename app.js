12250d0f902b395cc7c70ab7b562bf7245470528
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-30 23:04:21 -0500

    MongoDB, mongoose & Schema 🐉

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/Blog');
 
 const app = express();
+
+// mongoDB connection
+const mongoURI =
+  'mongodb+srv://test123:test123@cluster0-t0uam.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) =>
+    app.listen(3000, () => console.log('Server running on port 3000...'))
+  )
+  .catch((err) => console.log('Error connecting to MongoDB'));
 
 // logger middleware
 app.use(morgan('dev'));
@@ -12,22 +24,23 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi's egg hunt",
-      description: 'lorem ipsum random text wysiwyg',
-    },
-    {
-      title: 'Mario finds stars',
-      description: 'lorem ipsum random text wysiwyg',
-    },
-    {
-      title: 'How to defeat bowser',
-      description: 'lorem ipsum random text wysiwyg',
-    },
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
+
+/* app.get('/all-blogs', async (req, res) => {
+  const blog = new Blog({
+    title: 'How to defeat Bowser',
+    snippet: 'Bowser defeat',
+    body: 'lorem ipsum random text',
+  });
+
+  try {
+    const result = await blog.save();
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
+}); */
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
@@ -37,6 +50,13 @@ app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create' });
 });
 
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => res.render('index', { title: 'Home', blogs: result }))
+    .catch((err) => console.log('Error fetching blogs', err));
+});
+
 app.get('/about-us', (req, res) => {
   res.redirect('/about');
 });
@@ -44,5 +64,3 @@ app.get('/about-us', (req, res) => {
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
-
-app.listen(3000, () => console.log('Server running on port 3000...'));
