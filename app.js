5b64a883c72b436fc6296dda83c116b5ed499070
Vishesh Singh <VisheshSingh16@gmail.com>
2020-07-01 22:30:39 -0500

    delete request 🗑

--- app.js
+++ app.js
@@ -78,6 +78,15 @@ app.get('/blogs/:id', (req, res) => {
   );
 });
 
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => console.log(err));
+});
+
 app.get('/about-us', (req, res) => {
   res.redirect('/about');
 });
